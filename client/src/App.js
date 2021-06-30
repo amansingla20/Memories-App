d6ac1cee56cfb2029a3100bf889580222ff9efc4
AMAN SINGLA <amansingla222@gmail.com>
2021-06-30 18:02:42 +0530

    Project initialised and backend work started

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Initialised</h1>
+      <h2>Start Working</h2>
+    </div>
+  );
+}
+
+export default App;
