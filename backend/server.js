a5777e8971f9b9d76930359568a0b5f40e3ee1bf
AMAN SINGLA <amansingla222@gmail.com>
2021-06-30 18:16:46 +0530

    routes initialized

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 // app.use(bodyParser.json({ limit: "30mb", extended: true }));
 // app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json());
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://new_user:newpassword@cluster0.ksvel.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
